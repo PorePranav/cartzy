c252898595481700c980be2dfa15385f2ca1cece
Pranav Pore <pranav@pranavpore.com>
2024-09-17 01:14:12 +0530

    initial setup

---
+++ src/main/java/com/pranavpore/cartzy/CartzyApplication.java
@@ -0,0 +1,13 @@
+package com.pranavpore.cartzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartzyApplication.class, args);
+	}
+
+}

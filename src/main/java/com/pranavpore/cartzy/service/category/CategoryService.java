5da132f2d793c05f6723f5702545b9508e143bc6
Pranav Pore <pranav@pranavpore.com>
2024-09-17 23:40:27 +0530

    implemented category service

---
+++ src/main/java/com/pranavpore/cartzy/service/category/CategoryService.java
@@ -0,0 +1,63 @@
+package com.pranavpore.cartzy.service.category;
+
+import com.pranavpore.cartzy.exceptions.ResourceAlreadyExistsException;
+import com.pranavpore.cartzy.exceptions.ResourceNotFoundException;
+import com.pranavpore.cartzy.model.Category;
+import com.pranavpore.cartzy.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    }
+
+    @Override
+    public Category getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category addCategory(Category category) {
+        return Optional
+                .of(category)
+                .filter(c -> !categoryRepository.existsByName(c.getName()))
+                .map(categoryRepository::save)
+                .orElseThrow(() -> new ResourceAlreadyExistsException("Category " + category.getName() + " already exists"));
+
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long id) {
+       return Optional
+               .ofNullable(getCategoryById(id))
+               .map(oldCategory -> {
+                   oldCategory.setName(category.getName());
+                   return categoryRepository.save(oldCategory);
+               }).orElseThrow(() -> new ResourceNotFoundException("Category not found"));
+    }
+
+    @Override
+    public void deleteCategory(Category category) {
+        categoryRepository
+                .findById(category.getId())
+                .ifPresentOrElse(categoryRepository::delete, () -> {
+                    throw new ResourceNotFoundException("Category not found");
+                });
+    }
+}
---
+++ src/main/java/com/pranavpore/cartzy/service/category/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.pranavpore.cartzy.service.category;
+
+import com.pranavpore.cartzy.model.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category getCategoryById(Long id);
+    Category getCategoryByName(String name);
+    List<Category> getAllCategories();
+    Category addCategory(Category category);
+    Category updateCategory(Category category, Long id);
+    void deleteCategory(Category category);
+}
